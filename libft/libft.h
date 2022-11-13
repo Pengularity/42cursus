491033aa0efe87501c6282fb6bd533ec92d98a00
Will <pengularity@gmail.com>
2022-11-13 23:40:00 +0100

    maj part 1 libft

--- libft/libft.h
+++ libft/libft.h
@@ -15,6 +15,8 @@
 
 # include <stddef.h>
 
+/* Part 1 */
+
 int					ft_isalpha(int c);
 int					ft_isdigit(int c);
 int					ft_isalnum(int c);
@@ -30,10 +32,14 @@ size_t				ft_strlcpy(char *dst, const char *src, size_t size);
 char				*ft_strchr(const char *s, int c);
 char				*ft_strrchr(const char *s, int c);
 int					ft_strncmp(const char *s1, const char *s2, size_t n);
+char				*ft_strnstr(const char	*big, const char *little, size_t len);
+char				*ft_strdup(const char *str);
 
 void				*ft_memset(void *s, int c, size_t n);
 void				ft_bzero(void *s, size_t n);
 void				*ft_memcpy(void *dest, const void *src, size_t n);
 void				*ft_memmove(void *dest, const void *src, size_t n);
+int					ft_memcmp(const void *s1, const void *s2, size_t n)
+void				*ft_calloc(size_t number, size_t size)
 
 #endif
