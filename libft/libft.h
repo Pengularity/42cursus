d850620215026cebac68bfb16e7f84ab50e8e05b
Will <pengularity@gmail.com>
2022-11-16 17:36:34 +0100

    maj libft

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: wnguyen <wnguyen@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/13 21:10:09 by wnguyen           #+#    #+#             */
-/*   Updated: 2022/11/15 17:53:31 by wnguyen          ###   ########.fr       */
+/*   Updated: 2022/11/16 17:36:06 by wnguyen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,5 +50,6 @@ char		*ft_substr(char const *s, unsigned int start, size_t len);
 char		*ft_strjoin(char const *s1, char const *s2);
 char		*ft_strtrim(char const *s1, char const *set);
 char		*ft_itoa(int n);
+char		**ft_split(char const *s, char c);
 
 #endif
