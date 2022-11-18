72d79530612f0c7625a3b26385564d08a9828251
Will <pengularity@gmail.com>
2022-11-18 01:51:31 +0100

    bonus part

--- libft/libft.h
+++ libft/libft.h
@@ -16,6 +16,12 @@
 # include <stddef.h>
 # include <stdlib.h>
 
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
+
 /* Part 1 */
 
 int			ft_isalpha(int c);
@@ -57,5 +63,9 @@ void		ft_putchar_fd(char c, int fd);
 void		ft_putstr_fd(char *s, int fd);
 void		ft_putendl_fd(char *s, int fd);
 void		ft_putnbr_fd(int n, int fd);
+
+/* Bonus Part */
+
+
 
 #endif
