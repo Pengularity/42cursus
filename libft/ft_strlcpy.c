03ab2d17ab25cc2a9832f49323189ce6c6e20bcd
Will <pengularity@gmail.com>
2022-11-23 18:09:56 +0100

    OK

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: wnguyen <wnguyen@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/13 21:09:20 by wnguyen           #+#    #+#             */
-/*   Updated: 2022/11/23 18:08:52 by wnguyen          ###   ########.fr       */
+/*   Updated: 2022/11/23 18:09:42 by wnguyen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 		return (0);
 	if (size != 0)
 	{
-		while (src[i] != '\0' && i < (dstsize - 1))
+		while (src[i] != '\0' && i < (size - 1))
 		{
 			dst[i] = src[i];
 			i++;
